e5fd50dd90412eb8cefa9b224558e8da5397deb7
KyoungMin <Min@kyungmins-Air.hitronhub.home>
2019-02-05 22:13:16 -0800

    VM set up vm started but fatal error occur

--- deliverables/scripted/wp_vm_setup.sh
+++ deliverables/scripted/wp_vm_setup.sh
@@ -1,65 +1,40 @@
-vboxmanage () { VBoxManage.exe "$@"; }
-
-# get the absolute path of the current script 
-declare script_path="$(python -c "import os; print(os.path.realpath('$0'))")"
-
-# get the path of its enclosing directory us this to setup relative paths
-declare script_dir="$(dirname ${script_path})"
-
-# Cludge to get the path of the directory where the vbox file is stored: creates file adjacent to vbox file
-# vboxmanage showvminfo displays line with the path to the config file -> grep "Config file returns it
-# the extended regex `(/[^/]+)+' matches everything that is a path i.e. / followed  by anthing not / 
-
-# You will need to change the VM name to match one of your VM's
+#Declare variables
+declare network_name="sys_net_prov"
 declare vm_name="test"
-
-declare vm_info="$(vboxmanage showvminfo "${vm_name}")"
-declare vm_conf_line="$(echo "${vm_info}" | grep "Config file")"
-declare vm_conf_file="$( echo "${vm_conf_line}" | grep -oE '(/[^/]+)+')"
-declare vm_directory="$(dirname "${vm_conf_file}")"
-
-echo "${vm_directory}"
-
-declare vm_folder="testVM"
-declare size_in_mb="3600"
-declare ctrlr_1="ctrl1"
-declare ctrlr_2="ctrl2"
+declare size_in_mb=3600
+declare ctrlr_name1="ctrl1"
+declare ctrlr_name2="ctrl2"
 declare group_name=""
-declare memory_mb="1280"
-declare iso_file_path="/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
-
-vboxmanage list natnetworks
-
-vboxmanage natnetwork remove --netname ${network_name}
+declare memory_mb=1280
 
 vboxmanage createvm --name ${vm_name} --register
 
-vboxmanage createhd --filename "/"/${vm_name}.vdi \
+vboxmanage createhd --filename "/Users/Min/VirtualBox VMs/"/${vm_name}.vdi \
                     --size ${size_in_mb} -variant Standard
 
-vboxmanage storagectl ${vm_name} --name $ctrlr_1 --add "ide" --bootable on
-vboxmanage storagectl ${vm_name} --name $ctrlr_2 --add "sata" --bootable on
+vboxmanage storagectl ${vm_name} --name ${ctrlr_name1} --add "sata" --bootable on
+vboxmanage storagectl ${vm_name} --name ${ctrlr_name2} --add "ide" --bootable on
 
 vboxmanage storageattach ${vm_name} \
-            --storagectl ${ctrlr_1} \ 
+            --storagectl ${{ctrlr_name1}} \ 
             --port 00 \
             --device 00 \
             --type dvddrive \
-            --medium "/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
+            --medium "/Users/Min/Desktop/Joon/ACIT4640/CentOS-7-x86_64-Minimal-1810.iso"
+
 vboxmanage storageattach ${vm_name} \
-            --storagectl ${ctrlr_2} \ 
+            --storagectl ${{ctrlr_name2}} \ 
             --port 01 \
             --device 01 \
             --type dvddrive \
             --medium "/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
 
-
 vboxmanage storageattach ${vm_name} \
-            --storagectl $ctrlr_1 \
+            --storagectl ${ctrlr_name1} \
             --port 01 \
             --device 01 \
             --type hdd \
-            --medium "/Users/Min/VirtualBox VMs/test"/${vm_name}.vdi \
+            --medium "/Users/Min/VirtualBox VMs"/${vm_name}.vdi \
             --nonrotational on
 
 vboxmanage modifyvm ${vm_name}\
