ff5c0dd4c9a54987e8fe7b36626b901ed631cde7
KyoungMin <Min@kyungmins-Air.hitronhub.home>
2019-02-06 02:27:01 -0800

    fixed fatal error and final wp_vm_setup

--- deliverables/scripted/wp_vm_setup.sh
+++ deliverables/scripted/wp_vm_setup.sh
@@ -1,44 +1,45 @@
-#Declare variables
 declare network_name="sys_net_prov"
 declare vm_name="test"
+declare vm_folder="/Users/Min/VirtualBox VMs/test"
 declare size_in_mb=3600
-declare ctrlr_name1="ctrl1"
-declare ctrlr_name2="ctrl2"
-declare group_name=""
+declare ctrl_type1='sata'
+declare ctrl_type2='ide'
+declare ctrlr_name1='satactrl'
+declare ctrlr_name2='idectrl'
 declare memory_mb=1280
 
-vboxmanage createvm --name ${vm_name} --register
+VBoxManage createvm --name ${vm_name} --register
 
-vboxmanage createhd --filename "/Users/Min/VirtualBox VMs/"/${vm_name}.vdi \
+VBoxManage createhd --filename "/Users/Min/VirtualBox VMs/test"/${vm_name}.vdi \
                     --size ${size_in_mb} -variant Standard
 
-vboxmanage storagectl ${vm_name} --name ${ctrlr_name1} --add "sata" --bootable on
-vboxmanage storagectl ${vm_name} --name ${ctrlr_name2} --add "ide" --bootable on
+VBoxManage storagectl ${vm_name} --name ${ctrlr_name1} --add ${ctrl_type1} --bootable on
+VBoxManage storagectl ${vm_name} --name ${ctrlr_name2} --add ${ctrl_type2} --bootable on
 
-vboxmanage storageattach ${vm_name} \
-            --storagectl ${{ctrlr_name1}} \ 
+VBoxManage storageattach ${vm_name} \
+            --storagectl ${ctrlr_name1} \
             --port 00 \
             --device 00 \
             --type dvddrive \
-            --medium "/Users/Min/Desktop/Joon/ACIT4640/CentOS-7-x86_64-Minimal-1810.iso"
+            --medium '/Users/Min/VirtualBox VMs/CentOS-7-x86_64-Minimal-1810.iso'
 
-vboxmanage storageattach ${vm_name} \
-            --storagectl ${{ctrlr_name2}} \ 
+VBoxManage storageattach ${vm_name} \
+            --storagectl ${ctrlr_name2} \
             --port 01 \
             --device 01 \
             --type dvddrive \
             --medium "/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
 
-vboxmanage storageattach ${vm_name} \
-            --storagectl ${ctrlr_name1} \
+VBoxManage storageattach ${vm_name} \
+            --storagectl ${ctrlr_name2} \
             --port 01 \
             --device 01 \
             --type hdd \
-            --medium "/Users/Min/VirtualBox VMs"/${vm_name}.vdi \
+            --medium "/Users/Min/VirtualBox VMs/test"/${vm_name}.vdi \
             --nonrotational on
 
-vboxmanage modifyvm ${vm_name}\
-            --groups "${group_name}"\
+VBoxManage modifyvm ${vm_name}\
+            --groups ""\
             --ostype "RedHat_64"\
             --cpus 1\
             --hwvirtex on\
@@ -53,6 +54,6 @@ vboxmanage modifyvm ${vm_name}\
             --boot2 dvd\
             --boot3 none\
             --boot4 none\
-            --memory "${memory_mb}"
+            --memory ${memory_mb}
 
-vboxmanage startvm ${vm_name} --type gui
+VBoxManage startvm ${vm_name} --type gui
